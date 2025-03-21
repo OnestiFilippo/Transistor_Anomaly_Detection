e91dea9f4bd2a00a7ca8c9b088685141d50d4deb
OnestiFilippo <onestifilippo99@gmail.com>
2025-03-21 17:17:32 +0100

    code cleaning

--- main.py
+++ main.py
@@ -8,32 +8,34 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Imposta il livello di log su ERROR
 import random
 
 # TODO: 
-# 1. Load the dataset images
-# 2. Preprocess the images (resize, normalize)
-# 3. Create the generative model
-# 4. Create the discriminative model
-# 5. Train the GAN
-# 6. Load the test images
-# 7. Preprocess the test images
-# 8. Generate images using the trained model
-# 9. Compare generated images with the test images
+# 01. Load the dataset images (DONE)
+# 02. Preprocess the images (resize, normalize) (DONE)
+# 03. Create the generative model
+# 04. Create the discriminative model
+# 05. Train the GAN
+# 06. Load the test images
+# 07. Preprocess the test images
+# 08. Generate images using the trained model
+# 09. Compare generated images with the test images
 # 10. Classify the generated images using the model trained in the previous task
 # 11. Evaluate the classification accuracy
 
-# Load the dataset images that are in the form of numpy arrays
+# ---------------------- 01. Load the dataset images ----------------------
+
+# Load the images that are in the form of numpy arrays
 dataset_path = "transistor/train/good"
 
-# Dimensioni target per il ridimensionamento
+# Images dimensions
 img_height, img_width = 1024, 1024
 
-# Caricare e preprocessare le immagini
+# Loading images from a folder in grayscale and normalizing them
 def load_images_from_folder(folder):
     images = []
     for filename in os.listdir(folder):
         if filename.endswith(".png"):  # Controlla che sia un JPG
             img_path = os.path.join(folder, filename)
             img = tf.keras.preprocessing.image.load_img(img_path, target_size=(img_height, img_width), color_mode='grayscale')
-            img_array = tf.keras.preprocessing.image.img_to_array(img) / 255.0
+            img_array = tf.keras.preprocessing.image.img_to_array(img)
             images.append(img_array)
     return np.array(images)
 
@@ -41,17 +43,24 @@ def load_images_from_folder(folder):
 X_train = load_images_from_folder(dataset_path)
 print(f"Dataset shape: {X_train.shape}")
 
+# Visualize a random image
 im = random.randint(0, X_train.shape[0])
-
 # Visualize 1024x1024 image and 128x128 image together
 plt.figure(figsize=(10, 5))
 plt.subplot(1, 2, 1)
-plt.imshow(X_train[im], cmap='gray')
+plt.imshow(X_train[im])
 plt.subplot(1, 2, 2)
-plt.imshow(tf.image.resize(X_train[im], [128, 128]), cmap='gray')
+plt.imshow(tf.image.resize(X_train[im], [128, 128]))
 plt.show()
+
+# ---------------------- 02. Preprocess the images ----------------------
 
 # Resize the images
 X_train = tf.image.resize(X_train, [128, 128])
+
+# Normalize the images
+X_train = X_train / 255.0
+
 print(f"Dataset shape: {X_train.shape}")
 
+# ---------------------- 03. Create the generative model ----------------------
